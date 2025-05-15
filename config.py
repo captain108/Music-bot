3167c373c16f28d746aa7ae752c3837a855bcb12
captain108 <papajiop11@gmail.com>
2025-05-15 17:36:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,4 @@
-import os
-
-API_ID = int(os.getenv("26904619"))
-API_HASH = os.getenv("b2c9026254d15f18286d6eaf2b3602bb")
-BOT_TOKEN = os.getenv("7512277994:AAEwK3SkXZEUDAMwe4A-JeU9e600HTUUafQ")
-OWNER_ID = int(os.getenv("5944513375"))
+API_ID = 26904619           # Your Telegram API ID (int)
+API_HASH = "b2c9026254d15f18286d6eaf2b3602bb" # Your Telegram API Hash (str)
+BOT_TOKEN = "7512277994:AAEwK3SkXZEUDAMwe4A-JeU9e600HTUUafQ" # Your bot token from @BotFather (str)
+OWNER_ID = 5944513375       # Your Telegram user ID (int)
