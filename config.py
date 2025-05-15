773e9c310cd70c667233715d2ba32ed2c9979778
captain108 <papajiop11@gmail.com>
2025-05-15 17:15:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
-API_ID = 12345678  # Replace with your API ID
-API_HASH = "your_api_hash"
-BOT_TOKEN = "your_bot_token"
-OWNER_ID = 123456789  # Replace with your Telegram user ID
+import os
+
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+OWNER_ID = int(os.getenv("OWNER_ID"))
